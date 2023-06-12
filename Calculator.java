00a986361326f651e126d1d0467ccb9440348c6c
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-06-12 12:43:45 -0600

    Adding TODOs to Calculator class

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,20 @@
+// This class is a simple calculator that takes two numbers from user input and performs the user chosen arithmetic operation on those two numbers and then displays the answer to the user.
 public class Calculator {
     public static void main (String [] args){
-        System.out.print("Hello World");
+        // TODO #1: Create a scanner object for capturing user input
+
+        // TODO #2: Create 2 variable to hold user input for each number for the calculator
+
+        // TODO #3: Ask the user to enter the first number, save info into variable
+
+        // TODO #4: Ask the user to enter the second number, save info into variable
+
+        // TODO #5: Ask the user which operation they would like the calculator to perform on the two numbers- add, subtract, multiply, divide
+
+        // TODO #6: Based on the user input from #5, execute the operation on the 2 given numbers.
+
+        // TODO #7: Display the result to the user
+
     }
 
 }
